eb94f0976f4ebe8ef4546a8c5bba1fdbe079a2ba
mostafa-j303 <mostafajarjour18@gmail.com>
2026-05-17 20:23:51 +0300

    add hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   allowedDevOrigins: ["192.168.100.251"],
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation";
+import { LoginPanel } from "@/components/LoginPanel";
 
 export default function Home() {
-  redirect("/login");
+  return <LoginPanel />;
 }
